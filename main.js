8f35c29e34405ff8e17b404d68298259210578b6
Cássius Borges <C.T.Borges@outlook.com>
2024-01-25 15:41:41 -0400

    functions for the buttons to make sounds

---
+++ main.js
@@ -0,0 +1,53 @@
+function playSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick =  playSomPom;
+
+function playSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick =  playSomClap;
+
+function playSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick =  playSomTim;
+
+function playSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick =  playSomPuff;
+
+function playSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick =  playSomSplash;
+
+function playSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick =  playSomToim;
+
+function playSomTPsh () {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick =  playSomTPsh;
+
+function playSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick =  playSomTic;
+
+function playSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick =  playSomTom;
